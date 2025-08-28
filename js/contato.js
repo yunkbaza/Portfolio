876544b53217384321a73d8491cbe466b4b88a7a
yunkbaza <allanbamirati@live.com>
2025-08-28 00:30:40 -0300

    JS para enviar uma mensagem via whatsapp

---
+++ js/contato.js
@@ -0,0 +1,14 @@
+function enviarWhats(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '5511947573410';
+
+    const texto = `Olá! Me Chamo ${nome}, ${mensagem}`;
+    const msgFormatada = encodeURIComponent(texto);
+
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+    window.open(url, '_blanck');
+}
